5b3b5b1b5fbb058b849f8d067a307f39019be83f
nathantbissell <nathantbissell@gmail.com>
2018-12-17 13:58:06 -0500

    restaurant owners cannot create ratings after update

--- app/routes/rating_routes.js
+++ app/routes/rating_routes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const passport = require('passport')
 
 const Rating = require('../models/rating')
+const Restaurant = require('../models/restaurant.js')
 
 const handle = require('../../lib/error_handler')
 const customErrors = require('../../lib/custom_errors')
+
 
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
@@ -13,7 +15,7 @@ const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
-router.get('/ratings', requireToken, (req, res) => {
+router.get('/ratings', (req, res) => {
   Rating.find()
     .then(ratings => {
       return ratings.map(rating => rating.toObject())
@@ -22,7 +24,7 @@ router.get('/ratings', requireToken, (req, res) => {
     .catch(err => handle(err, res))
 })
 
-router.get('/ratings/:id', requireToken, (req, res) => {
+router.get('/ratings/:id', (req, res) => {
   Rating.findById(req.params.id)
     .then(handle404)
     .then(rating => res.status(200).json({ rating: rating.toObject() }))
@@ -32,12 +34,32 @@ router.get('/ratings/:id', requireToken, (req, res) => {
 router.post('/ratings', requireToken, (req, res) => {
   req.body.rating.owner = req.user.id
 
-  Rating.create(req.body.rating)
-    .then(rating => {
-      res.status(201).json({ rating: rating.toObject() })
-    })
-    .catch(err => handle(err, res))
-})
+  Restaurant.findById(req.body.rating.restaurant)
+    .then(function (restaurant) {
+      const restOwner = restaurant.owner
+        // console.log('User ID:             ' + req.body.rating.owner)
+        // console.log('Restaurant Owner ID: ' + restOwner)
+        if (restOwner == req.body.rating.owner) {
+          console.log('print error')
+        } else {
+          Rating.create(req.body.rating)
+          .then(rating => {
+            res.status(201).json({ rating: rating.toObject() })
+          })
+          .catch(err => handle(err, res)) 
+        }
+      }
+    )
+  })
+
+  // if (restOwner === req.body.rating.owner) {
+  //   console.log('matching')
+  // } else {
+  //   Rating.create(req.body.rating)
+  //   .then(rating => {
+  //     res.status(201).json({ rating: rating.toObject() })
+  //   })
+  //   .catch(err => handle(err, res))
 
 router.patch('/ratings/:id', requireToken, (req, res) => {
   delete req.body.rating.owner
